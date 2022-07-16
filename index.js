72b051e5ff069b03d79ce6632f9034e8dc45ae1b
Meka Seba <mekasebameka@gmail.com>
2022-07-16 02:49:02 -0700

    it all seems good, I even changed the name on the js second file and it prompted to update export n did but in the console it says it cannot import statement outside of module?

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
-   
-function addressMaker(address) {
-    const {city, state} = address;
-            
-    const newAddress = {
-        city, 
-        state,
-        country, 'United States'
-    };
-    console.log(`${newAddress.city}, ${newAddress.state}, ${newAddress.country}`)  
-}
-    
-addressMaker({city: 'Austin', state: 'Texas'});
+import { add } from './data.js';
 
+let result = add(3, 2);
+
+console.log(result);
